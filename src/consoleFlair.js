3c0a576b9a2b2d13d9c6384f0829b2fb5e79d2e9
Hans Meyer <ps_github@hansifer.com>
2013-05-14 23:54:08 +0200

    Update consoleFlair.js

--- src/consoleFlair.js
+++ src/consoleFlair.js
@@ -20,5 +20,3 @@ function styledConsoleLog() {
 
     console.log.apply(console, argArray);
 }
-
-styledConsoleLog('<span style="color:hsl(0, 100%, 90%);background-color:hsl(0, 100%, 50%);"> Red </span> <span style="color:hsl(39, 100%, 85%);background-color:hsl(39, 100%, 50%);"> Orange </span> <span style="color:hsl(60, 100%, 35%);background-color:hsl(60, 100%, 50%);"> Yellow </span> <span style="color:hsl(120, 100%, 60%);background-color:hsl(120, 100%, 25%);"> Green </span> <span style="color:hsl(240, 100%, 90%);background-color:hsl(240, 100%, 50%);"> Blue </span> <span style="color:hsl(300, 100%, 85%);background-color:hsl(300, 100%, 25%);"> Purple </span> <span style="color:hsl(0, 0%, 80%);background-color:hsl(0, 0%, 0%);"> Black </span>');
